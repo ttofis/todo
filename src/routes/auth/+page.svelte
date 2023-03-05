8c326546e1cea8e7d3817784fe3ccae589490f75
Christoforos Charalambous <chrischaralambous14@gmail.com>
2023-03-05 03:53:37 +0200

    Client approach

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+    import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
+    import { auth } from "$lib/firebase";
+    import { browser } from "$app/environment";
+    import type { PageData } from './$types';
+    import { goto } from "$app/navigation";
+
+    export let data: PageData;
+
+    if (isSignInWithEmailLink(auth, data.url) && browser) {
+        let email = window.localStorage.getItem('emailForSignIn');
+        while (!email) {
+            email = window.prompt('Please provide your email for confirmation');
+        }
+        signInWithEmailLink(auth, email, data.url)
+        .then(() => {
+            window.localStorage.removeItem('emailForSignIn');
+            goto("/");
+        }).catch(() => {
+            goto("/");
+        });
+    }
+</script>
+
 <h1>Trying to log you in</h1>
--- src/routes/auth/+page.ts
+++ src/routes/auth/+page.ts
@@ -1,24 +1,5 @@
 import type { PageLoad } from "./$types";
-import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
-import { auth } from "$lib/firebase";
-import { redirect } from "@sveltejs/kit";
-import { browser } from "$app/environment";
-import { error } from "@sveltejs/kit";
 
 export const load = (( url ) => {
-    console.log(url.url.toString());
-    if (isSignInWithEmailLink(auth, url.url.toString()) && browser) {
-        let email = window.localStorage.getItem('emailForSignIn');
-        while (!email) {
-            email = window.prompt('Please provide your email for confirmation');
-        }
-        signInWithEmailLink(auth, email, url.url.toString())
-        .then(() => {
-            window.localStorage.removeItem('emailForSignIn');
-            throw redirect(300, '/');
-        }).catch((err) => {
-            throw error(500, err.message);
-        });
-    }
-    return;
+    return { url: url.url.toString() };
 }) satisfies PageLoad;
