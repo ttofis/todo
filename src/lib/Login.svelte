130eee3679a4469563da9bc1beacaa2be8a29b39
Christoforos Charalambous <chrischaralambous14@gmail.com>
2023-03-05 17:31:26 +0200

    Better Google login

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { validateEmail } from "$lib/validateEmail";
     import { triggerToast } from "$lib/triggerToast";
-    import { GoogleAuthProvider, signInWithPopup, sendSignInLinkToEmail } from "firebase/auth";
+    import { GoogleAuthProvider, sendSignInLinkToEmail, signInWithCredential  } from "firebase/auth";
     import { auth } from "$lib/firebase";
+    import { dev } from "$app/environment";
+    import { onMount } from "svelte";
     
-    const provider = new GoogleAuthProvider();
-
     let email: string;
     let disabled = false;
 
@@ -24,7 +24,7 @@
             })
             .catch((err) => {
                 triggerToast("Something went wrong, please try again", 'variant-filled-warning');
-                console.log(err);
+                if (dev) console.log(err);
             })
             disabled = false;
         }else{
@@ -32,38 +32,49 @@
         }
     }
 
-    /*async function login() {
-        const auth = await pb.collection(Collections.Users).authWithPassword(email, password);
-        if (browser) setTimeout(() => {}, 1000);
-        return auth;
-    }
+    function handleCredentialResponse(response: { credential: any; }) {
+        const idToken = response.credential;
+        const credential = GoogleAuthProvider.credential(idToken);
 
-    async function signup() {
-        const data = {
-            "email": email,
-            "emailVisibility": true,
-            "password": password,
-            "passwordConfirm": password
-        };
-        await pb.collection(Collections.Users).create(data);
-        await pb.collection(Collections.Users).requestVerification(email);
-    }*/
-
-    function handleLoginGoogle() {
-        signInWithPopup(auth, provider)
-        .catch((err) => {
+        signInWithCredential(auth, credential).catch((err) => {
             triggerToast("Something went wrong, please try again", 'variant-filled-warning');
-            console.log(err);
+            if (dev) console.log(err);
         });
     }
+
+    onMount(() => {
+        //@ts-ignore
+        window.handleCredentialResponse = handleCredentialResponse;
+    })
 </script>
 
-<h2>Welcome to <span class="todo">Todo</span></h2>
-<h2>the slick task app</h2>
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client"></script>
+</svelte:head>
+
+<div id="g_id_onload"
+    data-client_id="580166982159-894t8sf8g269ee5pru36kf77dv0d8s5r.apps.googleusercontent.com"
+    data-context="signin"
+    data-ux_mode="popup"
+    data-callback="handleCredentialResponse"
+    data-auto_prompt="false">
+</div>
+
+<div class="text-center">
+    <h2>Welcome to <span class="todo">Todo</span></h2>
+    <h2>the slick task app</h2>
+</div>
 <form class="mt-2 w-full flex flex-col gap-3" on:submit|preventDefault>
     <input class="input" type="email" placeholder="email" autocomplete="email" bind:value={email} required />
     <button type="submit" disabled={disabled} class="btn variant-glass-secondary" on:click={handleLogin}>Login</button>
-    <button type="button" disabled={disabled} class="btn variant-glass-primary" on:click={handleLoginGoogle}>Sign in with Google</button>
+    <div class="g_id_signin"
+        data-type="standard"
+        data-shape="pill"
+        data-theme="outline"
+        data-text="signin_with"
+        data-size="large"
+        data-logo_alignment="left">
+    </div>
 </form>
 
 <style>
