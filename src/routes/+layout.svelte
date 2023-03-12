54b06befd9c70aed9904c4f358e3152e1e890105
Christoforos Charalambous <chrischaralambous14@gmail.com>
2023-03-12 02:57:40 +0200

    Last attempt to fix Safari mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 	<!--<script src="https://accounts.google.com/gsi/client"></script>-->
 </svelte:head>
 
-<div class="h-screen w-screen overflow-hidden">
+<div style="height: -webkit-fill-available;" class="h-screen w-screen overflow-hidden overscroll-none">
 	<Header />
 	<slot />
 </div>
